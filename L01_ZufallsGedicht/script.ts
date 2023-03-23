1997341d56167c8493b63240bcf4d561ea1a41c7
LauraKupferschmid <116755408+LauraKupferschmid@users.noreply.github.com>
2023-03-23 13:03:17 +0100

    Update script.ts

--- L01_ZufallsGedicht/script.ts
+++ L01_ZufallsGedicht/script.ts
@@ -1,6 +1,24 @@
 namespace randomPoem {
 
-console.log("huhu");
+    let subjekte: string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let prädikate: string [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objekte: string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+    console.log(subjekte, prädikate, objekte);
+
+    for (let i: number = subjekte.length; i > 0; i--) {
+        console.log(i);
+    }
+
+    function getVerse(): void {
+        console.log("alohomora");
+    }
+
+    console.log(getVerse);
+
+
+
+
 
 
 
