17360b6b2d9d2242fdc8eea4fc46b7eb39659c00
LauraKupferschmid <116755408+LauraKupferschmid@users.noreply.github.com>
2023-03-23 13:03:14 +0100

    Update script.js

--- L01_ZufallsGedicht/script.js
+++ L01_ZufallsGedicht/script.js
@@ -1,6 +1,16 @@
 "use strict";
 var randomPoem;
 (function (randomPoem) {
-    console.log("huhu");
+    let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let prädikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    console.log(subjekte, prädikate, objekte);
+    for (let i = subjekte.length; i > 0; i--) {
+        console.log(i);
+    }
+    function getVerse() {
+        console.log("alohomora");
+    }
+    console.log(getVerse);
 })(randomPoem || (randomPoem = {}));
 //# sourceMappingURL=script.js.map
