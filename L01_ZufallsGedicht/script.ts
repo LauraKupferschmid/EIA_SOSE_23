c7fadff36a5c79fb301d1f8fcaa0ec471ecbc155
LauraKupferschmid <116755408+LauraKupferschmid@users.noreply.github.com>
2023-03-24 21:33:00 +0100

    Update script.ts

--- L01_ZufallsGedicht/script.ts
+++ L01_ZufallsGedicht/script.ts
@@ -17,22 +17,22 @@ namespace randomPoem {
     function getVerse(_subjekte: string[], _prädikate: string[], _objekte: string[]): string {
         // console.log("alohomora"); 
 
-        let zeile: string = "";
-        let zahl1: number = Math.floor(Math.random() * _subjekte.length);
-        zeile = zeile + _subjekte[zahl1] + " ";
-        _subjekte.splice(zahl1, 1);
+        let vers: string = "";
+        let number1: number = Math.floor(Math.random() * _subjekte.length);
+        vers = vers + _subjekte[number1] + " ";
+        _subjekte.splice(number1, 1);
 
-        let zahl2: number = Math.floor(Math.random() * _prädikate.length);
-        zeile = zeile + _prädikate[zahl2] + " ";
-        _prädikate.splice(zahl2, 1);
+        let number2: number = Math.floor(Math.random() * _prädikate.length);
+        vers = vers + _prädikate[number2] + " ";
+        _prädikate.splice(number2, 1);
 
-        let zahl3: number = Math.floor(Math.random() * _objekte.length);
-        zeile = zeile + _objekte[zahl3] + " ";
-        _objekte.splice(zahl3, 1);
+        let number3: number = Math.floor(Math.random() * _objekte.length);
+        vers = vers + _objekte[number3] + " ";
+        _objekte.splice(number3, 1);
 
-        console.log(zeile + " ");
+        console.log(vers + " ");
 
-        return zeile;
+        return vers;
 
     }
 
