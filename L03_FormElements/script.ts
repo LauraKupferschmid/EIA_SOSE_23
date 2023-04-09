5a03ef6deaf13805eacc18149863c34c75162682
LauraKupferschmid <116755408+LauraKupferschmid@users.noreply.github.com>
2023-04-09 17:48:36 +0200

    Update script.ts

--- L03_FormElements/script.ts
+++ L03_FormElements/script.ts
@@ -1,7 +1,32 @@
 namespace aufgabe3 {
 
+/*
+Aufgabe: <L03 FormElements>
+Name: <Laura Kupferschmid>
+Matrikel: <272613>
+Datum: <09.04.2023>
+Quellen: <Madeleine Hansen> 
+*/
+
+window.addEventListener('load', handleLoad);
+
+function handleLoad(): void {
+    document.getElementById("new")!.addEventListener('click', newtask);
+    document.getElementById("edit")!.addEventListener('click', edittask);
+    document.getElementById("add")!.addEventListener('click', addtask);
+};
+
+function newtask() {
+    console.log("Bitte erstelle eine Aufgabe")
+};
+
+function addtask() {
+    console.log("Hinzufügen einer Aufgabe")
+};
+
+function edittask() {
+    console.log("Bearbeite die Aufgabe")
+};
 
 
-
-    
 }
