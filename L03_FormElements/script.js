e6c20120be4e6c57b1956709319b8db68e8f7e83
LauraKupferschmid <116755408+LauraKupferschmid@users.noreply.github.com>
2023-04-09 17:48:32 +0200

    Update script.js

--- L03_FormElements/script.js
+++ L03_FormElements/script.js
@@ -1,2 +1,31 @@
 "use strict";
+var aufgabe3;
+(function (aufgabe3) {
+    /*
+    Aufgabe: <L03 FormElements>
+    Name: <Laura Kupferschmid>
+    Matrikel: <272613>
+    Datum: <09.04.2023>
+    Quellen: <Madeleine Hansen>
+    */
+    window.addEventListener('load', handleLoad);
+    function handleLoad() {
+        document.getElementById("new").addEventListener('click', newtask);
+        document.getElementById("edit").addEventListener('click', edittask);
+        document.getElementById("add").addEventListener('click', addtask);
+    }
+    ;
+    function newtask() {
+        console.log("Bitte erstelle eine Aufgabe");
+    }
+    ;
+    function addtask() {
+        console.log("Hinzufügen einer Aufgabe");
+    }
+    ;
+    function edittask() {
+        console.log("Bearbeite die Aufgabe");
+    }
+    ;
+})(aufgabe3 || (aufgabe3 = {}));
 //# sourceMappingURL=script.js.map
