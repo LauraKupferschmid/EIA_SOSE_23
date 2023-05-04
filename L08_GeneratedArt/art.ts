be421decd1e0a4536940d72535a9472017e2070f
LauraKupferschmid <116755408+LauraKupferschmid@users.noreply.github.com>
2023-05-04 10:44:25 +0200

    Create art.ts

---
+++ L08_GeneratedArt/art.ts
@@ -0,0 +1,29 @@
+namespace generatedart{
+  
+    window.addEventListener("load",hndload)
+
+    function hndload() {
+        console.log("loaded");
+    }
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("mycanvas");
+    let crc2: CanvasRenderingContext2D =<CanvasRenderingContext2D>canvas.getContext("2d");
+
+    crc2.fillStyle = "#FF00FA";
+    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+
+    crc2.beginPath();
+    crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
+    crc2.closePath();
+    crc2.stroke();
+    
+    crc2.beginPath();
+    crc2.ellipse(40, 50, 30, 50, 15, 30, 20)
+    crc2.closePath();
+    crc2.stroke();
+    
+    crc2.beginPath();
+    crc2.bezierCurveTo(160, 100, 70, 130, 50, 130)
+    crc2.closePath();
+    crc2.stroke();
+
+}
